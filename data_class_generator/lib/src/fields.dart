d0475b4a6e67219784c6ef5181a6a516a8c16138
Vsevolod Krasnov <vsevolod.krasnov@gmail.com>
2020-07-06 11:58:43 +0300

    Fixing inheritance

--- data_class_generator/lib/src/fields.dart
+++ data_class_generator/lib/src/fields.dart
@@ -1,38 +1,26 @@
-// Copyright (c) 2017, Google Inc. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 import 'dart:collection';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:built_collection/built_collection.dart';
 
-/// Gets fields, including from interfaces. Fields from interfaces are only
-/// returned if they are not also implemented by a mixin.
-///
-/// If a field is overridden then just the closest (overriding) field is
-/// returned.
 BuiltList<FieldElement> collectFields(ClassElement element) =>
     collectFieldsForType(element.thisType);
 
-/// Gets fields, including from interfaces. Fields from interfaces are only
-/// returned if they are not also implemented by a mixin.
-///
-/// If a field is overridden then just the closest (overriding) field is
-/// returned.
 BuiltList<FieldElement> collectFieldsForType(InterfaceType type) {
   var fields = <FieldElement>[];
   // Add fields from this class before interfaces, so they're added to the set
   // first below. Re-added fields from interfaces are ignored.
   fields.addAll(_fieldElementsForType(type));
 
-  if (type.superclass != null) {
-    //todo allSupertypes?
-    fields.addAll(_fieldElementsForType(type.superclass));
+  var superclass = type.superclass;
+  while (superclass != null && superclass.name != 'Object') {
+    fields.addAll(_fieldElementsForType(superclass));
+
+    superclass = superclass.superclass;
   }
-//  Set<InterfaceType>.from(type.superclass)
-//    ..addAll(type.mixins)
+//  Set<InterfaceType>.from(element.allSupertypes)
+//    ..addAll(element.type.mixins)
 //    ..forEach((interface) => fields.addAll(collectFieldsForType(interface)));
 
   // Overridden fields have multiple declarations, so deduplicate by adding
